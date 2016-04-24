2812c1c6139ddb59263d2beb9ca5be6f841a2339
Qrex <471597503@qq.com>
2016-04-25 00:21:29 +0800

    test

--- controller/hook.js
+++ controller/hook.js
@@ -1,9 +1,13 @@
+var fyscu = require('../libs/fyscu.js');
+var code = require('../libs/code.js');
+
 var hook = {
     name:"web hook"
 };
 
 hook.hook = function (req, res) {
     console.log(req.body);
+    res.end(fyscu.out(code.success));
     return;
 };
 
