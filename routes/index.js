01fda041443be45c6baadeeb1d9045b1c3a55234
Qrex <471597503@qq.com>
2016-04-25 00:05:13 +0800

    /hook

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var hook = require('../controller/hook.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.post('/hook',function (req, res) {
+router.post('/hook', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    hook.hook(req,res);
+    hook.hook(req, res);
 });
 
 
