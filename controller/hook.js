706c2644cbd13de9e279c78c3010f4c890941541
Qrex <471597503@qq.com>
2016-04-25 17:49:37 +0800

    git.oschina

--- controller/hook.js
+++ controller/hook.js
@@ -8,21 +8,42 @@ var hook = {
 };
 
 hook.hook = function (req, res) {
-    var hook = config.project[req.body.repository.name];
-    if(hook.language=='node'){
-        var shell = 'cd '+hook.href+' & git pull & pm2 restart '+hook.pm2name;
-    }else {
-        //shiyixia
-        var shell = 'cd '+hook.href+' & git pull ';
-    }
-    var childHook = child.exec(shell,function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
+    if(req.body.repository.name){
+        console.log(req.body);
 
-    })
+        if(hook.method=='github'){
+            var hook = config.project[req.body.repository.name];
+            if(hook.language=='node'){
+                var shell = 'cd '+hook.href+' & git pull & pm2 restart '+hook.pm2name;
+            }else {
+                //shiyixia
+                var shell = 'cd '+hook.href+' & git pull ';
+            }
+        }else if(hook.method=='oschina'&&hook.password==req.body.password){
+            var hook = config.project[req.body.hook_name];
+            if(hook.language=='node'){
+                var shell = 'cd '+hook.href+' & git pull & pm2 restart '+hook.pm2name;
+            }else{
+                var shell = 'cd '+hook.href+' & git pull ';
+            }
+        }else {
+            console.log("I don't know which platform or your password id is wrong");
+            res.end(fyscu.out(code.success));
+            return;
+        }
+        var childHook = child.exec(shell,function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        })
+    }else {
+        console.log(fyscu.out(code.paramError));
+        return;
+    }
+
+
 };
 
 module.exports = hook;
