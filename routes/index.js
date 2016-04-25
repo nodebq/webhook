4f7484064b73514d2a6699c0c6112ebb76af74c9
Qrex <471597503@qq.com>
2016-04-25 19:03:21 +0800

    /路径

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var hook = require('../controller/hook.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    // res.render('index', {title: 'Express'});
+    res.header("Access-Control-Allow-Origin", "*");
+    hook.hook(req, res);
 });
 
 router.post('/hook', function (req, res) {
