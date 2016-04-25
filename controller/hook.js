a6ef2901ef2e1c8c8c15de55b58d03a7e8609599
Qrex <471597503@qq.com>
2016-04-25 11:37:44 +0800

    雏形

--- controller/hook.js
+++ controller/hook.js
@@ -1,16 +1,26 @@
 var fyscu = require('../libs/fyscu.js');
 var code = require('../libs/code.js');
-var config = require('../config.js')
+var config = require('../config.js');
+var child = require('child_process');
 
 var hook = {
     name:"web hook"
 };
 
 hook.hook = function (req, res) {
-    if(req.body.repository.name == )
-    console.log(req.body.repository.name);
-    res.end(fyscu.out(code.success));
-    return;
+    var hook = config.project[req.body.repository.name];
+    if(hook.language=='node'){
+        var shell = 'cd '+hook.href+' & git pull & pm2 restart '+hook.pm2name;
+    }else {
+
+    }
+    var childHook = child.exec(shell,function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    })
 };
 
 module.exports = hook;
