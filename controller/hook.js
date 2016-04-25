e7e88eb6037bd78a13b700307881c39e48985ff7
Qrex <471597503@qq.com>
2016-04-25 10:41:17 +0800

    six

--- controller/hook.js
+++ controller/hook.js
@@ -1,12 +1,14 @@
 var fyscu = require('../libs/fyscu.js');
 var code = require('../libs/code.js');
+var config = require('../config.js')
 
 var hook = {
     name:"web hook"
 };
 
 hook.hook = function (req, res) {
-    console.log(req.body);
+    if(req.body.repository.name == )
+    console.log(req.body.repository.name);
     res.end(fyscu.out(code.success));
     return;
 };
